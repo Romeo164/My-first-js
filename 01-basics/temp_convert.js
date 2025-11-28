fcfc9491a1867ab1f520dcbc2e9b3e1190af60aa
Romeo164 <neupaneneeraz.uk@gmail.com>
2025-11-28 07:50:51 +0000

    new with string in temerature converter

--- 01-basics/temp_convert.js
+++ 01-basics/temp_convert.js
@@ -1,5 +1,5 @@
 function convertCtoF(value){
  const faranheit = value *(9/5)+32;
- return faranheit;
+ return ` your temperature in farenheit is ${faranheit} degree.`;
 }
 console.log(convertCtoF(-10));
